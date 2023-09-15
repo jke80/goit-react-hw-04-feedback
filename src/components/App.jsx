59d4a6f81761f4b7f229bec5777b36ef38e99957
Evhenii <kolomoycev81@gmail.com>
2023-09-15 16:55:53 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,68 +1,55 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Section } from './Section/Section';
 import { FeedBackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export const App = () => {
+  const [state, setState] = useState({ good: 0, neutral: 0, bad: 0 });
 
-  handelClick = event => {
-    this.setState(prevState => {
+  const handelClick = event => {
+    setState(prevState => {
       const { name } = event.target;
-      return { [name]: prevState[name] + 1 };
+      return { ...state, [name]: prevState[name] + 1 };
     });
   };
 
-  total() {
-    return this.state.good + this.state.bad + this.state.neutral;
-  }
+  const total = state.good + state.bad + state.neutral;
 
-  positivePercentage() {
-    if (!this.total()) return 0;
-    return Math.round((this.state.good / this.total()) * 100);
-  }
+  const positivePercentage = !total
+    ? 0
+    : Math.round((state.good / total) * 100);
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.total();
-    const positivePercentage = this.positivePercentage();
-    const options = Object.keys(this.state);
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          // justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Section title="Please leave feedback">
-          <FeedBackOptions
-            options={options}
-            onLeaveFeedback={this.handelClick}
+  const { good, neutral, bad } = state;
+  const options = Object.keys(state);
+
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        // justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Section title="Please leave feedback">
+        <FeedBackOptions options={options} onLeaveFeedback={handelClick} />
+      </Section>
+      {total ? (
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
           />
         </Section>
-        {total ? (
-          <Section title="Statistics">
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            />
-          </Section>
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </div>
-    );
-  }
-}
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </div>
+  );
+};
